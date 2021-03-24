1c04aa2542912a296c8ab925f38f20cd3636b215
Kamil <ckkomancz18@yahoo.com>
2021-03-24 11:55:13 +0100

    add pytest.ini file

--- math_func.py
+++ math_func.py
@@ -1,16 +1,3 @@
-
-
 def add(x, y=2):
     return x + y
     
-    
-def subtract(x, y=2):
-    return x - y
-    
-  
-def multiply(x, y=2):
-    return x * y
-    
-    
-def divide(x, y=2):
-    return x / y
